ad4e5f4d26a9fda550e21d70c1fd2628270d0a1b
MORGANBUCKLEYMCNAMAR <g00420537@atu.ie>
2024-11-06 16:12:50 +0000

    Github Created

---
+++ src/main/java/ie/atu/week7test/Customer.java
@@ -0,0 +1,13 @@
+package ie.atu.week7test;
+
+public class Customer {
+    public static void Customer{
+        public class Customer{
+            private String customerid;
+            private String name;
+            private String email;
+            private String phoneNumber;
+        }
+
+    }
+}
---
+++ src/main/java/ie/atu/week7test/CustomerAPP.java
@@ -0,0 +1,8 @@
+package ie.atu.week7test;
+
+public class CustomerApp {
+    public static void CustomerApp{
+
+
+    }
+}
